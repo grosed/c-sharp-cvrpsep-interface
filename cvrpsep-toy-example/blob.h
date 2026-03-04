6c7973441026d663d760a7c5ba0567b937579e0f
Daniel Grose <daniel.grose@mathsbox.com>
2026-03-04 11:45:55 +0000

    added a test file to the toy project

---
+++ cvrpsep-toy-example/blob.h
@@ -0,0 +1,22 @@
+
+#ifndef ___BLOB_H___
+#define ___BLOB_H___
+
+
+
+void blob(
+    int NoOfCustomers,
+    int* Demand,
+    int CAP,
+    int NoOfEdges,
+    int* EdgeTail,
+    int* EdgeHead,
+    double* EdgeX,
+    int MaxNoOfCuts,
+    double EpsForIntegrality
+	  );
+
+
+
+
+#endif
